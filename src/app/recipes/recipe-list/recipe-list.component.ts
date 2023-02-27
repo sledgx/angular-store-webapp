36eec2b5cc051e71626d2fc52d664a68b7dc9dac
sledgx <andrea.am85@gmail.com>
2023-02-27 15:31:09 +0100

    added recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simple a test',
+      'https://cdn.pixabay.com/photo/2012/04/01/16/51/pizza-23477__340.png'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simple a test',
+      'https://cdn.pixabay.com/photo/2012/04/01/16/51/pizza-23477__340.png'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simple a test',
+      'https://cdn.pixabay.com/photo/2012/04/01/16/51/pizza-23477__340.png'
+    )
+  ]
 }
