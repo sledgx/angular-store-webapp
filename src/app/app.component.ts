4e534561cdc6327bd38ab718949d191a230d3a09
sledgx <andrea.am85@gmail.com>
2023-03-18 15:36:46 +0100

    added simple component selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-store-webapp';
+  loadedFeature = 'recipes'
+  
+  onNavigare(feature: string) {
+    this.loadedFeature = feature
+  }
 }
